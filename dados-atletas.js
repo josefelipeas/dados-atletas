80d8f2e8190a4e530ed24ff61e1ec481c7965a92
José Felipe <josefelipeas1@gmail.com>
2025-12-19 12:49:11 -0400

    Implementando calculaCategoria()
    Implementando método calculaCategoria().

--- dados-atletas.js
+++ dados-atletas.js
@@ -7,7 +7,13 @@ class Atleta {
         this.notas = notas;
     }
     
-    calculaCategoria() {}
+    calculaCategoria() {
+        if(this.idade >= 9 && this.idade <= 11) return "Infantil";
+        else if(this.idade >= 12 && this.idade <= 13) return "Juvenil";
+        else if(this.idade >= 14 && this.idade <= 15) return "Intermediário";
+        else if(this.idade >= 16 && this.idade <= 30) return "Adulto";
+        else return "Sem categoria";
+    }
 
     calculaIMC() {}
 
