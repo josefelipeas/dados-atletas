e5b54ab4bdb92ea49c6c7b72a9d40f5a6bfc35f4
José Felipe <josefelipeas1@gmail.com>
2025-12-19 12:39:01 -0400

    Estabelecendo retornos da instancia em dados-atletas.js
    Utilizando console.log para estabelecer o retorno dos métodos de instância para o usuário.

--- dados-atletas.js
+++ dados-atletas.js
@@ -6,7 +6,7 @@ class Atleta {
         this.altura = altura;
         this.notas = notas;
     }
-
+    
     calculaCategoria() {}
 
     calculaIMC() {}
@@ -29,4 +29,12 @@ class Atleta {
 
 }
 
-const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log(atleta.obtemNomeAtleta());
+console.log(atleta.obtemIdadeAtleta());
+console.log(atleta.obtemPesoAtleta());
+console.log(atleta.obtemNotasAtleta());
+console.log(atleta.obtemCategoria());
+console.log(atleta.obtemIMC());
+console.log(atleta.obtemMediaValida());
